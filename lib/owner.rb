49a0877e7d4e36b8f5a6ed8c908832b65e577804
Milly Flores <miflo0h@gmail.com>
2020-06-16 05:24:49 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,63 +1,73 @@
- # code goes here
- class Owner
-  attr_accessor :name, :pets
-  attr_reader :species
+class Owner
+OWNERS = [] 
+attr_accessor :name, :pets 
+attr_reader :species 
 
-  @@owners = []
+def initialize(species)
+  @species = species 
+  OWNERS << self 
+  @pets = {:fishes => [], :dogs => [], :cats => []}
+end 
 
-  def initialize(species, pets ={:dogs => [], :cats => []})
-    @species = species
-    @pets = pets
-    @@owners << self
-  end
+def self.all ##Do we have to use this if we use a class variable? @@all 
+OWNERS 
+end 
 
-  def say_species
-    "I am a #{@species}."
-  end
+def self.reset_all 
+OWNERS.clear 
+end 
 
-  def buy_cat(cat_name)
-    @pets[:cats] << Cat.new(cat_name)
-  end
-
-  def buy_dog(dog_name)
-    @pets[:dogs] << Dog.new(dog_name)
-  end
-
-  def walk_dogs
-    @pets[:dogs].each do |dog|
-      dog.mood = "happy"
-    end
-  end
-
-  def play_with_cats
-    @pets[:cats].each do |cat|
-      cat.mood = "happy"
-    end
-  end
-
-  def sell_pets
-    @pets.each do |type, name_array|
-      name_array.each do |pet|
-        pet.mood = "nervous"
-        #name_array.delete(pet)
-      end 
-    end
-    @pets = {}
-  end
-
-  def list_pets
-    "I have #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length} cat(s)."
-  end
-
-  def self.all
-    @@owners
-  end
-
-  def self.reset_all
-    @@owners.clear
-  end
-
-  def self. count
-    @@owners.length
-  end
+def self.count
+OWNERS.size 
 end
+
+def species 
+  @species 
+end 
+
+def say_species 
+  return "I am a #{species}"
+end 
+  
+def name 
+  @name #where did we set @name = name? 
+end 
+
+def buy_fish(name)
+  pets[:fishes] << Fish.new(name) #how does owner.rb know where fish.rb is? 
+end 
+
+def buy_cat(name)
+  pets[:cats] << Cat.new(name)
+end 
+
+def buy_dog(name)
+  pets[:dogs] << Dog.new(name)
+end 
+
+def walk_dogs
+  pets[:dog].each do |dog|
+    dog.mood = "happy"
+  end 
+end 
+
+def play_with_cats
+  pets[:cats].each do |cat| 
+  cat.mood = "Happy"
+  end 
+end 
+
+def feed_fish
+  pets[:fish].each do |fish|
+  fish.mood = "happy"
+  end 
+end 
+
+def list_pets
+  "I have #{pets[:fishes].count} fish. I have #{pets[:cats].count} cats. I have #{pets[:dogs].count} dogs."
+ end 
+
+end #finish Owner 
+
+
+
