a9fed120a1d6b630d49060f943f61546a62ef3b4
Milly Flores <miflo0h@gmail.com>
2020-06-16 04:42:39 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -46,7 +46,7 @@
   end
 
   def list_pets
-    "I have #{@pets[:fishes].length} fish, #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length} cat(s)."
+    "I have #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length} cat(s)."
   end
 
   def self.all
